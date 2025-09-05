be7c54224aee96450860c6d174104717c9c4e57c
Arlo O'Keeffe <tiger@k5d.de>
2025-09-05 12:32:28 +0200

    map

--- spotify-csv-to-playlist.py
+++ spotify-csv-to-playlist.py
@@ -5,29 +5,27 @@ import pandas as pd
 from urllib.parse import urlparse
 from spotipy.oauth2 import SpotifyOAuth
 
-def find_spotify_id_for_artist_name(artist, name):
-    query = f"{artist} {name}"
-    results = sp.search(query)
-    tracks = results['tracks']['items']
-    #for i, track in enumerate(tracks):
-    #    print(f"{track}")
-    #     print(f'{', '.join([d['name'] for d in track['artists']])} - {track['name']}')
-    return tracks[0]['id']
-
-
-def find_spotify_id_for_song(spotify_mapping, song):
-    row = spotify_mapping.loc[spotify_mapping['apple_music_id'].astype(str) == song['apple_music_id'], 'spotify_id']
-    if row.empty:
-        spotify_id = find_spotify_id_for_artist_name(song['artist'], song['name'])
-        spotify_mapping.loc[len(spotify_mapping)] = {'apple_music_id': song['apple_music_id'], 'spotify_id': spotify_id}
-        spotify_mapping.to_csv("spotify-mapping.csv", index=False)
-        return spotify_id
-    return row.astype(str).iloc[0]
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='567ed2100ef746ff8bc4765c6fe21ac3',
                                                client_secret='9c2182d252b048bdb09ec8307842b455',
                                                redirect_uri='http://127.0.0.1:50872',
                                                scope='user-library-read,playlist-modify-private'))
+spotify_mapping = pd.read_csv("spotify-mapping.csv").set_index('apple_music_id')
+
+def find_spotify_id_for_artist_name(artist, name):
+    query = f"{artist} {name}"
+    results = sp.search(query)
+    tracks = results['tracks']['items']
+    return tracks[0]['id']
+
+def find_spotify_id_for_song(song):
+    amid = song['apple_music_id']
+    if amid in spotify_mapping.index:
+        return spotify_mapping.loc[amid, 'spotify_id']
+    else:
+        spotify_id = find_spotify_id_for_artist_name(song['artist'], song['name'])
+        spotify_mapping.loc[amid] = {'spotify_id': spotify_id}
+        spotify_mapping.to_csv("spotify-mapping.csv", index_label='apple_music_id')
+        return spotify_id
 
 playlist_id = '74eUXrePcNpIrEYaFBlmbw'
 results = sp.playlist_items(playlist_id)
@@ -37,13 +35,15 @@ while results['next']:
     results = sp.next(results)
     tracks.extend(results['items'])
 
-spotify_mapping = pd.read_csv("spotify-mapping.csv")
-
 df = pd.read_csv("songs.csv")
 
 for i, song in df.iterrows():
-    spotify_id = find_spotify_id_for_song(spotify_mapping, song)
-    print(f"{song['artist']}, {spotify_id}")
-    if not any(track['track']['id'] == spotify_id for track in tracks):
-        print("nope")
-        #sp.playlist_add_items(playlist_id, [f"spotify:track:{spotify_id}"])
+    spotify_id = find_spotify_id_for_song(song)
+    if spotify_id == '-1':
+        print(f"No spotify ID for {song['artist']} - {song['name']}")
+    elif any(track['track']['id'] == spotify_id for track in tracks):
+        print(f"Already in the playlist {song['artist']} - {song['name']}")
+        continue
+    else:
+        print(f"Adding {song['artist']} - {song['name']}")
+        sp.playlist_add_items(playlist_id, [f"spotify:track:{spotify_id}"])
