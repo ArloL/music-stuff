85a03d44341687722760eee2ce15bb34270adead
Arlo O'Keeffe <tiger@k5d.de>
2026-03-11 21:24:48 +0100

    make scripts work with change file names

--- shell.sh
+++ shell.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -o errexit
 
-#!/bin/sh
-set -o errexit
-
 rm -f scripts/tmp/beaTunes.*.db
 cp -c "${HOME}/Library/Application Support/beaTunes/Database/"beaTunes-*.h2.db \
     scripts/tmp/beaTunes.h2.db
 java -cp "/Applications/beaTunes5.app/Contents/Java/"h2-*.jar \
-    "org.h2.tools.Console" \
-    -url "jdbc:h2:~/Developer/beatunes-dbviewer/scripts/tmp/beaTunes" \
-    -user sa \
-    -password "" \
-    -browser \
-    -webPort 56547
+    "org.h2.tools.Shell" \
+    -help
