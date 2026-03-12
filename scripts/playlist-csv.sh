b294c8b086455da939a45424a75cd4ec9e1f371d
Arlo O'Keeffe <tiger@k5d.de>
2026-03-12 12:45:11 +0100

    sh

---
+++ scripts/playlist-csv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+uv run playlist-csv.py "Critical Mass 2025-08"
+uv run playlist-csv.py "Critical Mass Next"
+uv run playlist-csv.py "Would Play"
+uv run playlist-csv.py "Would Play And Didnt"
