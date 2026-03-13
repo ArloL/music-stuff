2c2d5e508ea77ea0a4ba2291aa64949d1ca59106
Arlo O'Keeffe <tiger@k5d.de>
2026-03-13 22:41:53 +0100

    scripts

---
+++ scripts/spotify.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# uv run spotify-playlist-delete-duplicates "Would Play"
+# uv run spotify-playlist-delete-duplicates Recommended
+# uv run spotify-playlist-delete-present-in-other-playlist Recommended "Would Play"
+# uv run spotify-playlist-delete-present-in-other-playlist Recommended Listened
