eccce09b3984de32c284e1474183bbe694c9ef32
Arlo O'Keeffe <tiger@k5d.de>
2026-03-14 23:52:07 +0100

    k

--- scripts/h2-console.sh
+++ scripts/h2-console.sh
@@ -6,14 +6,7 @@ set -o xtrace
 
 cd "$(dirname "$0")/" || exit 1
 
-rm -f ./tmp/beaTunes.*.db
-
-files=("${HOME}/Library/Application Support/beaTunes/Database/"beaTunes-*.h2.db)
-if (( ${#files[@]} != 1 )); then
-  echo "Expected exactly 1 db file, found ${#files[@]}" >&2
-  exit 1
-fi
-cp -c "${files[0]}" ./tmp/beaTunes.h2.db
+sh ./clone-dbs.sh
 
 java -cp "/Applications/beaTunes5.app/Contents/Java/"h2-*.jar \
     "org.h2.tools.Console" \
