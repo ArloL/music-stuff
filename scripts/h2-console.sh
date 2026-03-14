caed1ae2ab8022f034d8aa78a048f47ba1b47d03
Arlo O'Keeffe <tiger@k5d.de>
2026-03-14 22:42:59 +0100

    tmp

--- scripts/h2-console.sh
+++ scripts/h2-console.sh
@@ -6,18 +6,18 @@ set -o xtrace
 
 cd "$(dirname "$0")/" || exit 1
 
-rm -f "scripts/tmp/"beaTunes.*.db
+rm -f ./tmp/beaTunes.*.db
 
 files=("${HOME}/Library/Application Support/beaTunes/Database/"beaTunes-*.h2.db)
 if (( ${#files[@]} != 1 )); then
   echo "Expected exactly 1 db file, found ${#files[@]}" >&2
   exit 1
 fi
-cp -c "${files[0]}" scripts/tmp/beaTunes.h2.db
+cp -c "${files[0]}" ./tmp/beaTunes.h2.db
 
 java -cp "/Applications/beaTunes5.app/Contents/Java/"h2-*.jar \
     "org.h2.tools.Console" \
-    -url "jdbc:h2:~/Developer/beatunes-dbviewer/scripts/tmp/beaTunes" \
+    -url "jdbc:h2:~/Developer/beatunes-dbviewer/tmp/beaTunes" \
     -user sa \
     -password "" \
     -browser \
