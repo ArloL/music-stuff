fbdfef77e628863bc7e95ae34742040ba9bbee98
Arlo O'Keeffe <tiger@k5d.de>
2026-03-19 21:08:00 +0100

    scripts

--- scripts/playlist-csv.sh
+++ scripts/playlist-csv.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -o errexit
+set -o nounset
+set -o xtrace
+
 uv run playlist-csv "Critical Mass 2025-08"
 uv run playlist-csv "Critical Mass Next"
 uv run playlist-csv "Would Play"
--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# uv run spotify-playlist-delete-duplicates "Would Play"
-# uv run spotify-playlist-delete-duplicates Recommended
-# uv run spotify-playlist-delete-present-in-other-playlist Recommended "Would Play"
-# uv run spotify-playlist-delete-present-in-other-playlist Recommended Listened
+set -o errexit
+set -o nounset
+set -o xtrace
+
+uv run spotify-playlist-delete-duplicates "Would Play"
+uv run spotify-playlist-delete-duplicates Recommended
+uv run spotify-playlist-delete-present-in-other-playlist Recommended "Would Play"
+uv run spotify-playlist-delete-present-in-other-playlist Recommended Listened
