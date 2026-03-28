9a171c34321030e8c0acb317f6493d2179be54e7
Arlo O'Keeffe <tiger@k5d.de>
2026-03-28 15:02:31 +0100

    update export

--- scripts/playlist-csv.sh
+++ scripts/playlist-csv.sh
@@ -4,7 +4,14 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+uv run playlist-csv "Critical Mass 2025-01"
+uv run playlist-csv "Critical Mass 2025-03"
+uv run playlist-csv "Critical Mass 2025-05"
+uv run playlist-csv "Critical Mass 2025-06"
+uv run playlist-csv "Critical Mass 2025-07"
 uv run playlist-csv "Critical Mass 2025-08"
+uv run playlist-csv "Critical Mass 2025-09"
+uv run playlist-csv "Critical Mass 2026-03"
 uv run playlist-csv "Critical Mass Next"
 uv run playlist-csv "Would Play"
 uv run playlist-csv "Would Play And Didnt"
