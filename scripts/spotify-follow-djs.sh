42a89edc31e9604e9cced388dae37bd620fc8e74
Arlo O'Keeffe <tiger@k5d.de>
2026-05-29 12:11:14 +0200

    follow more djs

--- scripts/spotify-follow-djs.sh
+++ scripts/spotify-follow-djs.sh
@@ -11,17 +11,21 @@ BarryCantSwim="id:37i9dQZF1DWVbfVJ3tB3QO"
 BellaBoo="id:37i9dQZF1DWYZhaTaHlh27"
 BlackCoffee="id:37i9dQZF1DXc8VZ9nbXQ1z"
 Daphni="id:37i9dQZF1DWTQHB0vBt1AG"
+DJBoring="id:37i9dQZF1DX00RdhV73Dbe"
 DJHell="id:1iarTWt8gQd6gcAUTMmGEt"
 DJSeinfeld="id:37i9dQZF1DWVInERw2ZCfq"
+FredAgain="id:3cakkcelZt3C857q5YKPjF"
 GerdJanson="id:37i9dQZF1DX8A9EbZP1hul"
 GuiBoratto="id:37i9dQZF1DX4fgrmoIzHtd"
 HoneyDijon="id:37i9dQZF1DWTYPRTIhI2jZ"
 Jamiexx="id:37i9dQZF1DXcIJGgaOURNE"
 JaydaG="id:37i9dQZF1DX0F8FtqyAjxC"
 KellyLeeOwens="id:37i9dQZF1DWYMVjvqDxZQX"
+LaurentGarnier="id:37i9dQZF1DX68l5gg4hq38"
 Logic1000="id:37i9dQZF1DWWbEGrb2ydWx"
 MaceoPlex="id:37i9dQZF1DWUomyMFpoR0R"
 MarcelDettmann="id:37i9dQZF1DX2MrqV1P93C9"
+MayaJaneColes="id:37i9dQZF1DWZkxJwAfCZA3"
 MissKittin="id:37i9dQZF1DWW9AqVvhy1Jb"
 Modeselektor="id:37i9dQZF1DWXl7Y0piXYnl"
 NinaKraviz="id:37i9dQZF1DX717gvXLoUJP"
@@ -37,17 +41,21 @@ uv run spotify-browser-add-to-playlist --headless \
     "${BellaBoo}" \
     "${BlackCoffee}" \
     "${Daphni}" \
+    "${DJBoring}" \
     "${DJHell}" \
     "${DJSeinfeld}" \
+    "${FredAgain}" \
     "${GerdJanson}" \
     "${GuiBoratto}" \
     "${HoneyDijon}" \
     "${Jamiexx}" \
     "${JaydaG}" \
     "${KellyLeeOwens}" \
+    "${LaurentGarnier}" \
     "${Logic1000}" \
     "${MaceoPlex}" \
     "${MarcelDettmann}" \
+    "${MayaJaneColes}" \
     "${MissKittin}" \
     "${Modeselektor}" \
     "${NinaKraviz}" \
